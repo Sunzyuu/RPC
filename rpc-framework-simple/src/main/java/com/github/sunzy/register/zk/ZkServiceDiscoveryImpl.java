7fbc44c29170939b613317efc0b67e9b00f617c2
sunzhengyu99 <sunzy9901@qq.com>
2023-08-03 23:09:33 +0800

    实现服务发现 ServiceDiscovery

--- rpc-framework-simple/src/main/java/com/github/sunzy/register/zk/ZkServiceDiscoveryImpl.java
+++ rpc-framework-simple/src/main/java/com/github/sunzy/register/zk/ZkServiceDiscoveryImpl.java
@@ -1,5 +1,7 @@
 package com.github.sunzy.register.zk;
 
+import com.github.sunzy.loadbalance.LoadBalance;
+import com.github.sunzy.loadbalance.loadbalancer.RandomLoadBalance;
 import com.github.sunzy.register.ServiceDiscovery;
 import com.github.sunzy.remoting.dto.RpcRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +14,16 @@ import java.net.InetSocketAddress;
  */
 @Slf4j
 public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
-//    private final LoadBalance loadBalance;
+    private final LoadBalance loadBalance;
+
+    public ZkServiceDiscoveryImpl() {
+        this.loadBalance = new RandomLoadBalance();
+    }
 
     @Override
-    public InetSocketAddress lookupService(RpcRequest request) {
+    public InetSocketAddress lookupService(RpcRequest rpcRequest) {
+        String rpcServiceName = rpcRequest.getRpcServiceName();
+
         return null;
     }
 }
