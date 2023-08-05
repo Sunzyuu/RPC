7e2308890002f8a06c5cbe4909c422330dca1623
sunzhengyu99 <sunzy9901@qq.com>
2023-08-05 21:42:21 +0800

    SocketRpcServer

---
+++ rpc-framework-simple/src/main/java/com/github/sunzy/remoting/transport/socket/SocketRpcServer.java
@@ -0,0 +1,22 @@
+package com.github.sunzy.remoting.transport.socket;
+
+import com.github.sunzy.factory.SingletonFactory;
+import com.github.sunzy.provider.ServiceProvider;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author sunzy
+ * @date 2023/8/5 21:37
+ */
+@Slf4j
+public class SocketRpcServer {
+    private final ExecutorService threadPool;
+    private final ServiceProvider serviceProvider;
+    public SocketRpcServer() {
+        threadPool = ThreadPoolFactoryUtil.createCustomThreadPoolIfAbsent("socket-server-rpc-pool");
+        serviceProvider = SingletonFactory.getInstance(ZkServiceProviderImpl.class);
+    }
+
+}
