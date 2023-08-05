1aff12ecd95bbcf2962e69136e20ef1018b2d6b3
sunzhengyu99 <sunzy9901@qq.com>
2023-08-05 21:36:44 +0800

    SocketRpcClient

--- rpc-framework-simple/src/main/java/com/github/sunzy/remoting/transport/socket/SocketRpcClient.java
+++ rpc-framework-simple/src/main/java/com/github/sunzy/remoting/transport/socket/SocketRpcClient.java
@@ -1,5 +1,7 @@
 package com.github.sunzy.remoting.transport.socket;
 
+import com.github.sunzy.enums.ServiceDiscoveryEnum;
+import com.github.sunzy.extension.ExtensionLoader;
 import com.github.sunzy.register.ServiceDiscovery;
 import com.github.sunzy.remoting.dto.RpcRequest;
 import com.github.sunzy.remoting.transport.RpcRequestTransport;
@@ -7,9 +9,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 /**
+ * 基于socket传输RpcRequest
  * @author sunzy
  * @date 2023/8/3 21:36
  */
@@ -17,19 +23,25 @@ import java.net.Socket;
 @Slf4j
 public class SocketRpcClient implements RpcRequestTransport {
 
-//    private final ServiceDiscovery serviceDiscovery;
+    private final ServiceDiscovery serviceDiscovery;
 
-
+    public SocketRpcClient() {
+        this.serviceDiscovery = ExtensionLoader.getExtensionLoader(ServiceDiscovery.class).getExtension(ServiceDiscoveryEnum.ZK.getName());
+    }
 
 
     @Override
     public Object sendRpcRequest(RpcRequest request){
-
+        InetSocketAddress inetSocketAddress = serviceDiscovery.lookupService(request);
         try(Socket socket = new Socket()) {
-
+            //socket connect server
+            socket.connect(inetSocketAddress);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(request);
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            return objectInputStream.readObject();
         } catch (Exception e) {
-
+            throw new RuntimeException(e);
         }
-        return null;
     }
 }
