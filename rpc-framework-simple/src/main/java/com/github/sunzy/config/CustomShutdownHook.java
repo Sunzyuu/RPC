506e87b80a91a801edcff1225dd46f0006650100
sunzhengyu99 <sunzy9901@qq.com>
2023-08-14 01:00:52 +0800

    添加 CustomShutdownHook

---
+++ rpc-framework-simple/src/main/java/com/github/sunzy/config/CustomShutdownHook.java
@@ -0,0 +1,37 @@
+package com.github.sunzy.config;
+
+import com.github.sunzy.register.zk.util.CuratorUtils;
+import com.github.sunzy.remoting.transport.socket.NettyRpcServer;
+import com.github.sunzy.utils.concurrent.ThreadPoolFactoryUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+/**
+ * When the server  is closed, do something such as unregister all services
+ * @author sunzy
+ * date 2023-08-14
+ */
+@Slf4j
+public class CustomShutdownHook {
+
+    private static final CustomShutdownHook CUSTOM_SHUTDOWN_HOOK = new CustomShutdownHook();
+
+    public static CustomShutdownHook getCustomShutdownHook() {
+        return CUSTOM_SHUTDOWN_HOOK;
+    }
+
+    public void clearAll() {
+        log.info("addShutdownHook for clearAll");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                InetSocketAddress inetSocketAddress = new InetSocketAddress(InetAddress.getLocalHost().getHostAddress(), NettyRpcServer.PORT);
+                CuratorUtils.clearRegistry(CuratorUtils.getZkClient(), inetSocketAddress);
+            } catch (UnknownHostException ignored) {
+            }
+            ThreadPoolFactoryUtil.shutDownAllThreadPool();
+        }));
+    }
+}
