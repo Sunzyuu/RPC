60ac9c08535d64202e531f83e0c061f4ab647806
sunzhengyu99 <sunzy9901@qq.com>
2023-08-13 22:05:03 +0800

    添加序列化模块Protostuff

---
+++ rpc-framework-simple/src/main/java/com/github/sunzy/serialize/protostuff/ProtostuffSerializer.java
@@ -0,0 +1,37 @@
+package com.github.sunzy.serialize.protostuff;
+
+import com.github.sunzy.serialize.Serializer;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * @author sunzy
+ * date 2023-08-13
+ */
+public class ProtostuffSerializer implements Serializer {
+    private static final LinkedBuffer BUFFER = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+
+
+    @Override
+    public byte[] serialize(Object obj) {
+        Class<?> clazz = obj.getClass();
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        byte[] bytes;
+        try {
+            bytes = ProtostuffIOUtil.toByteArray(obj, schema, BUFFER);
+        } finally {
+            BUFFER.clear();
+        }
+        return bytes;
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+}
