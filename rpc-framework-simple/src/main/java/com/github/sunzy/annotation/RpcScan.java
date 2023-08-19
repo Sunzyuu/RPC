9e3f131818c8f4b3dc23240edc1d50d749232848
sunzhengyu99 <sunzy9901@qq.com>
2023-08-20 00:21:50 +0800

    添加 CustomScanner

---
+++ rpc-framework-simple/src/main/java/com/github/sunzy/annotation/RpcScan.java
@@ -0,0 +1,16 @@
+package com.github.sunzy.annotation;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * @author sunzy
+ * date 2023-08-20
+ */
+//@Target({ElementType.TYPE, ElementType.METHOD})
+//@Retention(RetentionPolicy.RUNTIME)
+//@Import(CustomScannerRegistrar.class)
+//@Documented
+//@interface RpcScan {
+//}
